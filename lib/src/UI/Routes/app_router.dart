847c38856daa3e39ed262515c9567bdb2843cf03
niiko235 <nicolaseplaza@gmail.com>
2025-09-27 11:17:59 -0500

    create all files to start coding

---
+++ lib/src/UI/Routes/app_router.dart
@@ -0,0 +1,17 @@
+
+// library;
+
+// import 'package:go_router/go_router.dart';
+
+// import 'routes_config.dart';
+
+// /// Instancia global del router de navegación para la aplicación.
+// /// Utiliza las rutas definidas en [appRoutes] y establece la vista de login como inicial.
+// final GoRouter appRouter = GoRouter(
+//   routes: appRoutes,
+//   initialLocation: '/home',
+//   debugLogDiagnostics: true,
+// );
+
+
+
---
+++ lib/src/UI/Routes/route_names.dart
@@ -0,0 +1,16 @@
+/// ### Nombre de las rutas como constantes
+/// ### archivo: route_names.dart
+/// Definimos el nombbre de las rutas como constantes 
+/// evitamos errores 
+
+
+
+
+abstract class RouteNames {
+  static const home = 'home'; // Pagina de inicio 
+  static const dashboard = 'dashboard'; // pagina listado de las investigacion y carrusel con los pajaros
+  static const investigation = 'investigation'; // Pagina informacion de una investigacion y el listado de sus muestreos 
+  static const sampling = 'sampling'; //  Pagina de un muestreo y listado de puntos de muestreo
+  static const sampligPoint = 'sampling-point'; // Pagina de un punto de muestreo y listado de especies en ese punto
+  static const specie = 'specie'; // Pagina de la especie seleccionada 
+}
